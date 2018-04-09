947f8bd7daa85f5fc2151bdd7b4bf774fb4bc2b6
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-09 03:12:24 -0500

    more related models

--- lib/handler.py
+++ lib/handler.py
@@ -1,7 +1,6 @@
 from .logger import log
 import simplejson as json
 from importlib import import_module
-from .models import Subscription, User
 
 
 class Handler:
@@ -18,33 +17,7 @@ class Handler:
         if parsed_event is None:
             return
 
-        # for sub in self.get_subscribers(parsed_event):
         self.get_handler('Email').publish(parsed_event)
-
-    def get_subscribers(self, event):
-        ''' Reads and groups subscriptions to an event '''
-        channel  = event['channel']
-
-        def filter_events(sub):
-            return sub.event == '*' or sub.event == event['event']
-
-        subs = {}
-
-        for sub in filter(filter_events, Subscription.tree_match('channel', channel)):
-            if sub.handler not in subs:
-                subs[sub.handler] = {
-                    'channel': channel,
-                    'event': event['event'],
-                    'users': [sub.proxy.user.get().to_json()],
-                    'handler': sub.handler,
-                    'params': [sub.params],
-                    'data': event['data'] if 'data' in event else dict(),
-                }
-            else:
-                subs[sub.handler]['users'].append(sub.proxy.user.get().to_json())
-                subs[sub.handler]['params'].append(sub.params)
-
-        return subs.values()
 
     def get_handler(self, name):
         if name not in self.handlers:
